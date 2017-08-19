58643b190421b1bdfb948838d893efd11f9255c4
aschen8 <andyshoakichen@gmail.com>
2017-08-19 01:07:02 -0700

    made some changes

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -12,16 +12,16 @@ var spanWinner     = document.getElementById("spanWinner");
 var outcomes       = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
                       'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var spanGuessesRemaining = document.getElementById("spanGuessesRemaining");
+var spanUserGuessed= ("spanUserGuessed");
 
-// var counter= 0;
-
-//     function incrementCount(){
-//        count++;
-//     }
-
-//     console.log(count);
-//     incrementCount();
-//     console.log(count);  
+// initialize(firstTime)
+// {   
+//     userWins=0;  userLosses=0; guessesRemaining = 12
+//     if(firstTime){  userWins=0;  userLosses=0; guessesRemaining = 12}
+// }
+//feel i need to create two funcitons: 1. initizalize function stating the 
+//starting parts of the game, and 2. a game over function that states game over
+//when user wins or loses 12 guesses and calls the initalize function
 
 document.onkeyup = function(event) {
 	var userChoice = event.key;
@@ -30,25 +30,33 @@ document.onkeyup = function(event) {
 
 	spanUserChoice.textContent= userChoice;
 
+
 	if(userChoice == computerChoice){
 
 		spanWinner.textContent= "you guessed correctly!";
 		userWins ++;
 		spanUserWins.textContent=userWins;
 		if(confirm("You've Won! Game Over, nothing to see here")){
-			window.location.reload()}
+			location.reload();}
+// location.reload works to reset the game, but need other way to reset it
+
+
 	}else {spanWinner.textContent="you guessed wrong!";
 	    userLosses ++;
 	    spanUserLosses.textContent= userLosses;
 	    guessesRemaining = 12 - userLosses;
 		spanGuessesRemaining.textContent= guessesRemaining;
+		//spanUserGuessed= spanUserChoice.join(',');
+		//userChoice= userChoice.join(',');
+		//spanUserChoice= userChoice;
+		//i feel i need to use .join to have game keep track of guesses letters. 
+		//not sure how to do this
 
-		userChoice = userChoice.join(',');
-
-		spanUserChoice = userChoice;
+		
 		
 		if(guessesRemaining == 0){alert("GAME OVERRRRRR");
-		window.location.reload();
+ 		location.reload();
+// location.reload works to reset the game, but need other way to reset it
 	     }
 
 		
